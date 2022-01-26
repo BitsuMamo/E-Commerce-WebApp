99f744695df4f8496d950a9ae4d725b028b90ddb
Bitsu Mamo <bitsua.mamo@gmail.com>
2022-01-26 12:05:50 +0300

    Fixed the promise thing. Was easy just forgot the await

--- js/backend/product.js
+++ js/backend/product.js
@@ -4,10 +4,8 @@ import getReviewsPerPoduct from "../backend/review.js";
 var baseUrl = "http://localhost/E-Commerce-Backend/php/";
 
 async function getProducts(type = "random") {
-  var ratings = getReviewsPerPoduct();
   var Products = [];
   var currentTime = Date.now();
-  // console.log(currentTime);
   var timeTillStgShowsUp = 311040000000;
   var jqxhr;
   try {
@@ -40,19 +38,17 @@ async function getProducts(type = "random") {
       });
       console.log(Products.length);
       break;
-    // This shit desn't work keeps sending a god damn promise instead of Data
+
     case "popular":
-      var r = Promise.resolve(ratings);
-      jqxhr.Data.filter((element) => {
-        if(parseInt((element.id) in ratings)){
-          return true;
+
+      var ratings = await getReviewsPerPoduct(50);
+
+      jqxhr.Data.forEach(element => {
+        if(element.id in ratings){
+          Products.push(new Product(element))
         }
-        return false;
-       }).forEach(element => {
-         Products.push(new Product(element));
-       });
-      console.log(r)
-      console.log(Products.length);
+      });
+      
       break;
      
     case "discount":
@@ -66,17 +62,10 @@ async function getProducts(type = "random") {
 
 
   }
-  /* console.log(Date.parse(jqxhr.Data[0].created_at))
-  console.log(currentTime)
-  console.log(jqxhr);
-  jqxhr.Data.forEach((element) => {
-    Products.push(new Product(element));
-  });
 
-  console.log(time); */
-  // console.log(Products);
   return Products;
 }
+
 async function getProductById(id = "1") {
   try {
     var jqxhr = await $.post(baseUrl + "select.php", {
